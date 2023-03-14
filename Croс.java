3c3782d6a58bb4a04c79ef9828b0781521375bf9
alinagudkova <127838181+alinagudkova@users.noreply.github.com>
2023-03-14 21:20:38 +0800

    Add files via upload

---
+++ Croс.java
@@ -0,0 +1,15 @@
+package croс;
+public class Croс {
+    public static void main(String[] args) {
+         for (int i = 1; i <= 100; i++) { /*перебор числел от 1 до 100*/
+            if (i % 3 == 0) { /*проверка кратности числа на 3*/
+                System.out.println("Fizz");
+            }else if (i % 5 == 0) { /*проверка кратности числа на 5*/
+                System.out.println("Buzz");
+            }else if (i % 3 == 0 && i % 5 == 0) { /*проверка кратности числа на 3 и 5*/
+                System.out.println("FizzBuzz");
+            }
+        }
+    }
+}
+
